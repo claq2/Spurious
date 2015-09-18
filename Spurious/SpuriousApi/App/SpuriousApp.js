bab23a0032c5822fcbf0ef5b67150e2526483d8b
James McLachlan <claquer@gmail.com>
2015-09-18 16:25:33 -0400

    Ready to build Angular app

---
+++ Spurious/SpuriousApi/App/SpuriousApp.js
@@ -0,0 +1,12 @@
+// Install the angularjs.TypeScript.DefinitelyTyped NuGet package 
+var SpuriousApp;
+(function (SpuriousApp) {
+    "use strict";
+    // Create the module and define its dependencies.
+    angular.module("SpuriousApp", [
+        // Angular modules 
+        "ngAnimate",
+        "ngRoute" // routing
+    ]);
+})(SpuriousApp || (SpuriousApp = {}));
+//# sourceMappingURL=SpuriousApp.js.map
---
+++ Spurious/SpuriousApi/App/SpuriousApp.ts
@@ -0,0 +1,17 @@
+// Install the angularjs.TypeScript.DefinitelyTyped NuGet package 
+
+module SpuriousApp {
+    "use strict";
+
+    // Create the module and define its dependencies.
+    angular.module("SpuriousApp", [
+        // Angular modules 
+        "ngAnimate", // animations
+        "ngRoute" // routing
+
+        // Custom modules 
+
+        // 3rd Party Modules
+    ]);
+}
+
