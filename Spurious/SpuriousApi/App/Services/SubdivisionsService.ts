cdcd5f427e3f77e1ac2e4650c313613b36228886
James McLachlan <claquer@gmail.com>
2015-09-22 17:11:24 -0400

    Starting services

---
+++ Spurious/SpuriousApi/App/Services/SubdivisionsService.ts
@@ -0,0 +1,21 @@
+﻿// Install the angularjs.TypeScript.DefinitelyTyped NuGet package
+module App {
+    "use strict";
+
+    interface ISubdivisionsService {
+        getData: () => string;
+    }
+    
+    class SubdivisionsService implements ISubdivisionsService {
+        static $inject: string[] = ["$http"];
+
+        constructor(private $http: ng.IHttpService) {
+        }
+
+        getData() {
+            return "";
+        }
+    }
+
+    angular.module("app").service("SubdivisionsService", SubdivisionsService);
+}
